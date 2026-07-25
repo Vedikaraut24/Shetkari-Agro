a5e9c529cad103e02366ec9d25ba399172bf56d6
vedikaraut24 <vedikaraut017@gmail.com>
2026-07-25 14:05:15 +0530

    Fix product service import export

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -2,11 +2,29 @@ import API from "./api";
 
 
 
+// Get all products
+
 export const getProducts = async()=>{
 
     const response =
     await API.get("/products");
 
+    return response.data;
+
+};
+
+
+
+
+// Create product
+
+export const createProduct = async(data)=>{
+
+    const response =
+    await API.post(
+        "/products",
+        data
+    );
 
     return response.data;
 
@@ -15,63 +33,69 @@ export const getProducts = async()=>{
 
 
 
+// Update product
 
-export const createProduct = async(data)=>{
+export const updateProduct = async(id,data)=>{
+
+    const response =
+    await API.put(
+        `/products/${id}`,
+        data
+    );
+
+    return response.data;
+
+};
+
+
+
+
+// Delete product
+
+export const deleteProduct = async(id)=>{
+
+    const response =
+    await API.delete(
+        `/products/${id}`
+    );
+
+    return response.data;
+
+};
+
+
+
+
+// Import CSV / Excel products
+
+export const importProducts = async(file)=>{
+
+
+    const formData = new FormData();
+
+
+    formData.append(
+        "file",
+        file
+    );
 
 
     const response =
     await API.post(
 
-        "/products",
+        "/products/import",
 
-        data
+        formData,
 
-    );
-
-
-    return response.data;
-
-
-};
-
-
-
-
-
-export const updateProduct = async(id,data)=>{
-
-
-    const response =
-    await API.put(
-
-        `/products/${id}`,
-
-        data
+        {
+            headers:{
+                "Content-Type":"multipart/form-data"
+            }
+        }
 
     );
 
 
     return response.data;
-
-
-};
-
-
-
-
-
-export const deleteProduct = async(id)=>{
-
-
-    const response =
-    await API.delete(
-
-        `/products/${id}`
-
-    );
-
-
-    return response.data;
-
 
 };
